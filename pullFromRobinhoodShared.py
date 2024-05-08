ebac3f87e6ebdc08ba6ada02e18a8e728b3704d5
kalishamaly <166883954+kalishamaly@users.noreply.github.com>
2024-05-07 19:06:05 -0700

    Update pullFromRobinhoodShared.py
    added in puts

--- pullFromRobinhoodShared.py
+++ pullFromRobinhoodShared.py
@@ -15,9 +15,10 @@ import robin_stocks.robinhood as robin
 # Define your Robinhood login credentials
 # Log in to Robinhood
 robin.login(email, password)
-minPrice = 1
-lowLim = -1
-upLim = 1
+minPrice = 0
+maxPrice = 4.5
+lowLim = -3
+upLim = 3
 # Define the path to the file containing ticker symbols
 file_path = "/Users/kalishamaly/Downloads/all_tickers.txt"
 
@@ -42,17 +43,21 @@ options_by_ticker = {}
 price_error_by_ticker = []
 call_options_data = {day: {} for day in fridays}
 call_options_data_short = {}
+
+put_options_data = {day: {} for day in fridays}
+put_options_data_short = {}
+
 stock_price = {}
 # Set a breakpoint
 # Iterate over each ticker
 for ticker in lines:
     try:
-        if len(ticker)<5:
+        if len(ticker)<6:
         # Fetch the latest stock price for the ticker
             stock_price[ticker] = robin.get_latest_price(ticker)
             rm_row = []
             # Iterate over each expiration day
-            if float(stock_price[ticker][0])>minPrice:
+            if maxPrice>float(stock_price[ticker][0])>minPrice:
                 for day in fridays:
                     # Fetch call options data for the ticker and expiration day
                     call_options = []
@@ -100,7 +105,57 @@ for ticker in lines:
                         if day_str not in call_options_data_short:
                             call_options_data_short[day_str] = []  # Initialize list if it doesn't exist
                         call_options_data_short[day_str].append(outFill)
+                    
+                    
+                    # Fetch call options data for the ticker and expiration day
+                    put_options = []
+                    put_options = robin.find_options_by_expiration(ticker, str(day),optionType='put')
+                    # Store call options data in the call_options_data dictionary
+                    #
+                    
+                    #Now to the puts
+                    put_options_data[day][ticker] = put_options
+                    strikePrice = []
+                    bidPrice = []
+                    diff = []
+                    for option in put_options:
+                        diff.append(float(option['strike_price']) - float(stock_price[ticker][0]))
+        
+                    # Find the indices of options with negative differences
+                    rm_row = [i for i, val in enumerate(diff) if val > 0]
+                    
+                    # Create new lists containing only the options with positive differences
+                    put_options_filtered = [put_options[i] for i in range(len(put_options)) if i not in rm_row]
+                    diff_filtered = [diff[i] for i in range(len(diff)) if i not in rm_row]
+                    
+                    # Update put_options and diff with the filtered lists
+                    put_options = put_options_filtered
+                    diff = diff_filtered
 
+                    # Get the sorted indices and values simultaneously
+                    sorted_indices_and_values = sorted(enumerate(diff), key=lambda x: x[1],reverse=True)
+                    
+                    # Separate sorted indices and values
+                    sorted_indices = [index for index, _ in sorted_indices_and_values]
+                    sorted_values = [value for _, value in sorted_indices_and_values]
+                    outFill = []
+                    tempOption = []
+                    if sorted_indices:  # Check if sorted_indices is not empty
+                        tempOption = put_options[sorted_indices[0]]
+                        outFill = [
+                            ticker,
+                            float(stock_price[ticker][0]),  # Convert to float
+                            float(tempOption['strike_price']),
+                            float(tempOption['bid_price']),
+                            (100 * (float(tempOption['bid_price']) / float(tempOption['strike_price'])))  # Convert to float before division
+                        ]
+                        # Assuming day is a datetime.date object
+                        day_str = str(day)  # Convert datetime.date to string
+                        
+                        # Append outFill to the list corresponding to the day
+                        if day_str not in put_options_data_short:
+                            put_options_data_short[day_str] = []  # Initialize list if it doesn't exist
+                        put_options_data_short[day_str].append(outFill)
                                 # print(f"Options data for ticker '{ticker}' processed successfully.")
     except Exception as e:
         # Handle errors and store the ticker in the price_error_by_ticker list
@@ -119,11 +174,14 @@ print(stock_price)
 print("Tickers with price errors:")
 print(price_error_by_ticker)
 #%%
-out_sorted = {}
+out_sorted_calls = {}
+out_sorted_puts = {}
 for day in call_options_data_short:
     day_str = str(day)  # Convert datetime.date to string
     
     # Append outFill to the list corresponding to the day
-    if day_str not in out_sorted:
-        out_sorted[day_str] = []  # Initialize list if it doesn't exist
-    out_sorted[day_str] = sorted(call_options_data_short[day_str], key=lambda x: x[4],reverse=True)  # Sort in descending order
+    if day_str not in out_sorted_calls:
+        out_sorted_calls[day_str] = []  # Initialize list if it doesn't exist
+        out_sorted_puts[day_str] = []  # Initialize list if it doesn't exist
+    out_sorted_calls[day_str] = sorted(call_options_data_short[day_str], key=lambda x: x[4],reverse=True)  # Sort in descending order
+    out_sorted_puts[day_str] = sorted(put_options_data_short[day_str], key=lambda x: x[4],reverse=True)  # Sort in descending order
