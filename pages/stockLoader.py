42de644f9656767411e2c23fefe9c6a08edf68fa
kalishamaly <166883954+kalishamaly@users.noreply.github.com>
2025-09-15 18:50:24 -0700

    Update stockLoader.py

--- pages/stockLoader.py
+++ pages/stockLoader.py
@@ -83,7 +83,6 @@ def handle_all_stock_actions(load_clicks, data_clicks, tickers):
             res = requests.get(sec_url, headers=headers)
             res.raise_for_status()
             data = res.json()
-            data = data[0:400]
             data = {k: data[k] for k in list(data.keys())}
 
             tickersLong = [item['ticker'] for item in list(data.values())]
@@ -94,7 +93,7 @@ def handle_all_stock_actions(load_clicks, data_clicks, tickers):
                     "Ticker": item.get("ticker", ""),
                     "Title": item.get("title", "")
                 }
-                for item in data.values()
+                for item in list(data.values())[:500]
             ]
 
             table = html.Div(
